7a902488177ebece5ef964ef2d0f735bcf54d0ab
Michael Schmidpeter <m4ffle@googlemail.com>
2022-02-24 00:15:19 +0100

    added thi_tools, currently containing time compare tools

---
+++ thi_tools/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'thi_tools'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='m1ch1',
+    maintainer_email='m4ffle@googlemail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+          'time_diff_now_node = thi_tools.time_diff_now:main',
+        ],
+    },
+)
